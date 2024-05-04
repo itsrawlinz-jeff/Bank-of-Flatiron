2ed1e77d2baaab7fd69b875dee6cfe5eedb2009b
Jeffrey Rowlings <rawlinzjeff264@gmail.com>
2024-05-04 14:59:17 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,7 @@
-// import 'semantic-ui-css/semantic.min.css';
-// import React from "react";
-// import ReactDOM from "react-dom";
-// import App from "./components/App";
-// import "semantic-ui-css/semantic.min.css";
-// import "./index.css";
-
-// ReactDOM.render(<App />, document.getElementById("root"));
-import 'semantic-ui-css/semantic.min.css';
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import App from "./components/App";
 import "semantic-ui-css/semantic.min.css";
 import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+ReactDOM.render(<App />, document.getElementById("root"));
